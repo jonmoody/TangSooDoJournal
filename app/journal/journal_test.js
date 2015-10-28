34dfc9351149acfef895b4d29c59a8a6d0ab7090
Jonathan Moody <jonmoody12@gmail.com>
2015-10-28 15:23:02 -0400

    Removed controller injection into beforeEach

--- app/journal/journal_test.js
+++ app/journal/journal_test.js
@@ -6,20 +6,22 @@ describe('tangSooDoJournal.journal module', function() {
 
   describe('journal controller', function(){
 
-    it('should get the current date', inject(function($controller) {
-      var homeController = $controller('JournalController');
-      var expectedDateString = 'November 27, 2015';
+    var journalController;
 
+    beforeEach(inject(function(_$controller_) {
+      journalController = _$controller_('JournalController');
+    }));
+
+    it('should get the current date', function() {
+      var expectedDateString = 'November 27, 2015';
       jasmine.clock().mockDate(new Date(expectedDateString));
 
-      expect(homeController.getCurrentDate()).toBe(expectedDateString);
-    }));
+      expect(journalController.getCurrentDate()).toBe(expectedDateString);
+    });
 
-    it('should contain hyung in the list of class activities', inject(function($controller) {
-      var homeController = $controller('JournalController');
-
-      expect(homeController.getClassActivities()).toContain('Hyung');
-    }));
+    it('should contain hyung in the list of class activities', function() {
+      expect(journalController.getClassActivities()).toContain('Hyung');
+    });
 
   });
 });
