9efdc97cd1bfb0ac52307d697b567e372233c71c
Jonathan Moody <jonmoody12@gmail.com>
2015-10-29 00:52:55 -0400

    Oops, forgot to add the directive. Added more test coverage

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -20,6 +20,17 @@ describe('Tang Soo Do Journal', function() {
 
     it('should display the list of class activities', function() {
       expect(element.all(by.repeater('activity in journal.getClassActivities()')).count()).toEqual(9);
+
+      var elements = element.all(by.css('.class-activities div label'));
+      expect(elements.get(0).getText()).toEqual('Hyung');
+      expect(elements.get(1).getText()).toEqual('Line Drill');
+      expect(elements.get(2).getText()).toEqual('Sparring');
+      expect(elements.get(3).getText()).toEqual('Targeting');
+      expect(elements.get(4).getText()).toEqual('Hand One Steps');
+      expect(elements.get(5).getText()).toEqual('Kick One Steps');
+      expect(elements.get(6).getText()).toEqual('Ho Sin Sul');
+      expect(elements.get(7).getText()).toEqual('Hyung Applications');
+      expect(elements.get(8).getText()).toEqual('Weapons');
     });
 
   });
