f5b03b10d38e421145d32bf7af3d738d69d6248d
Jonathan Moody <jonmoody12@gmail.com>
2015-11-07 19:57:38 -0500

    Added rest test for updating an existing record instead of adding extra records for the same day

--- rest-tests/activities_test.js
+++ rest-tests/activities_test.js
@@ -61,7 +61,6 @@ describe('activities rest api', function() {
         activities: expectedActivities
       })
       .end(function(error, response) {
-        expect(error).to.equal(null);
         expect(response.body._id).to.equal(expectedDate);
         expect(response.body.activities).to.eql(expectedActivities);
         done();
@@ -69,11 +68,44 @@ describe('activities rest api', function() {
 
     superagent.get('http://localhost:8000/api/load')
       .end(function(error, response) {
-        expect(error).to.equal(null);
-        expect(response.status).to.equal(200);
         expect(response.body.length).to.equal(1);
         expect(response.body[0]._id).to.equal(expectedDate);
         expect(response.body[0].activities).to.equal(expectedActivities);
+        done();
+      });
+  });
+
+  it('will update an existing record if the data was already posted on the same day', function(done) {
+    var expectedDate = 'January 15, 2015';
+    var originalActivities = [{
+      name: 'Hyung',
+      time: 15
+    }];
+    var updatedActivities = [{
+      name: 'Hyung',
+      time: 60
+    }];
+
+    superagent.post('http://localhost:8000/api/save')
+      .send({
+        _id: expectedDate,
+        activities: originalActivities
+      });
+
+    superagent.post('http://localhost:8000/api/save')
+      .send({
+        _id: expectedDate,
+        activities: updatedActivities
+      })
+      .end(function(error, response) {
+        done();
+      });
+
+    superagent.get('http://localhost:8000/api/load')
+      .end(function(error, response) {
+        expect(response.body.length).to.equal(1);
+        expect(response.body[0]._id).to.equal(expectedDate);
+        expect(response.body[0].activities).to.equal(updatedActivities);
         done();
       });
   });
