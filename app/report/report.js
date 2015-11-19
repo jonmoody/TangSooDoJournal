59cd66c93ea166f5206539281f8b5f73a964fabb
Jonathan Moody <jonmoody12@gmail.com>
2015-11-18 20:09:32 -0500

    Added test to get all activities for report

--- app/report/report.js
+++ app/report/report.js
@@ -9,6 +9,20 @@ angular.module('tangSooDoJournal.report', ['ngRoute'])
   });
 }])
 
-.controller('ReportController', function() {
+.controller('ReportController', function($http) {
+
+  var controller = this;
+
+  this.getActivities = function() {
+    $http({
+      method: 'GET',
+      url: '/api/load',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(function successCallback(response) {
+
+    }, undefined);
+  }
 
 });
--- app/report/report_test.js
+++ app/report/report_test.js
@@ -7,13 +7,40 @@ describe('tangSooDoJournal.report module', function() {
   describe('report controller', function() {
 
     var reportController;
+    var $httpBackend;
 
-    beforeEach(inject(function(_$controller_) {
+    beforeEach(inject(function(_$controller_, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
       reportController = _$controller_('ReportController');
     }));
 
     it('should create the report controller', function() {
       expect(reportController).not.toBeNull();
+    });
+
+    it('should retrieve the list of completed activites when the controller is created', function() {
+      var expectedData = {
+        data: [
+          {
+            _id: 'February 1, 2015',
+            activities: [{
+              name: 'Hyung',
+              time: 15
+            }]
+          },
+          {
+            _id: 'February 3, 2015',
+            activities: [{
+              name: 'Hyung',
+              time: 30
+            }]
+          }
+        ]
+      }
+
+      $httpBackend.whenGET('/api/load').respond(expectedData, '');
+      reportController.getActivities();
+      $httpBackend.flush();
     });
 
   });
